53d510110c376e722923ca738be87cf9fed6e5db
Danilo Camargo <dno_kaneda@hotmail.com>
2020-01-17 14:56:23 -0300

    Feat: CORS

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-<<<<<<< HEAD
 require('dotenv').config();
 
 const express = require('express');
 const nodemailer = require('nodemailer');
+var cors = require('cors')
+
 const app = express();
-const PORT = 8080;
+const PORT = 9000;
 const config = process.env;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors())
 
 let transporter = nodemailer.createTransport({
   service: config.SERVICE,
@@ -22,10 +24,14 @@ let transporter = nodemailer.createTransport({
 });
 
 app.post('/send-message', (req, res) => {
-  transporter.sendMail({  to:  req.body.to,
-                          subject: req.body.subject,
-                          text: req.body.text,
-                          html: req.body.html, }, function(err, data) {
+  transporter.sendMail({  to:       req.body.to,
+                          cc:       req.body.to,
+                          bcc:      req.body.bcc,
+                          subject:  req.body.subject,
+                          text:     req.body.text,
+                          html:     req.body.html, 
+                          // attachments: [{ path: 'data:text/plain;base64,aGVsbG8gd29ybGQ=' }]
+                        }, function(err, data) {
     if (err) {            
       return res.json({ Error: err });
     } else {      
@@ -35,41 +41,3 @@ app.post('/send-message', (req, res) => {
 });
 
 app.listen(PORT);
-=======
-require('dotenv').config();
-
-const express = require('express');
-const nodemailer = require('nodemailer');
-const app = express();
-const PORT = 8080;
-const config = process.env;
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-let transporter = nodemailer.createTransport({
-  service: config.SERVICE,
-  host: config.HOST,
-  port: config.PORT,
-  secure: config.SECURE,  
-  auth: {
-    user: config.EMAIL,
-    pass: config.PASSWORD
-  },
-});
-
-app.post('/send-message', (req, res) => {
-  transporter.sendMail({  to:  req.body.to,
-                          subject: req.body.subject,
-                          text: req.body.text,
-                          html: req.body.html, }, function(err, data) {
-    if (err) {            
-      return res.json({ Error: err });
-    } else {      
-      return res.json({ Msg: "Message sent!" });
-    }
-  });
-});
-
-app.listen(PORT);
->>>>>>> fcfa8cac9f6299925a741bcaed034951605e1e15
